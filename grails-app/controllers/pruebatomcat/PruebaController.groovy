a9983530c989d68ab5d11fc61c588b3bc7ee9e96
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-08-19 16:27:13 -0300

    Prueba conexion BD en produccion exitosa

---
+++ grails-app/controllers/pruebatomcat/PruebaController.groovy
@@ -0,0 +1,6 @@
+package pruebatomcat
+
+class PruebaController {
+
+    def scaffold = Prueba
+}
---
+++ grails-app/domain/pruebatomcat/Prueba.groovy
@@ -0,0 +1,11 @@
+package pruebatomcat
+
+class Prueba {
+
+    String nombre
+
+    static constraints = {
+        nombre(nullable: true)
+    }
+
+}
